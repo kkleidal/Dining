ad21dabf45c44db060a06170a56972eb435133a8
Ken Leidal <kkleidal@gmail.com>
2013-09-20 15:28:13 -0400

    Initial Commit
    Grabs rss feeds from bon appetit

---
+++ fetch.php
@@ -0,0 +1,15 @@
+<?php
+	$simmons = "http://legacy.cafebonappetit.com/rss/menu/402";
+	$maseeh = "http://legacy.cafebonappetit.com/rss/menu/398";
+	$baker = "http://legacy.cafebonappetit.com/rss/menu/399";
+	$mccormick = "http://legacy.cafebonappetit.com/rss/menu/400";
+	$next = "http://legacy.cafebonappetit.com/rss/menu/401";
+	$url = "";
+	$hall = $_GET['h'];
+	if (strcmp($hall, "simmons") == 0) $url = $simmons;
+	if (strcmp($hall, "maseeh") == 0) $url = $maseeh;
+	if (strcmp($hall, "baker") == 0) $url = $baker;
+	if (strcmp($hall, "mccormick") == 0) $url = $mccormick;
+	if (strcmp($hall, "next") == 0) $url = $next;
+	if ($url != "") echo file_get_contents($url);
+?>
